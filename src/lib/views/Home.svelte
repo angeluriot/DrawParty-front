7827cf39038ccda1d1a1c999e419fc01cbf04c00
angeluriot <angel.uriot@gmail.com>
2022-07-12 02:58:23 +0800

    tab

--- src/lib/views/Home.svelte
+++ src/lib/views/Home.svelte
@@ -9,13 +9,13 @@
 <Counter />
 
 <p>
-  Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-  apps.
+	Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
+	apps.
 </p>
 
 <p>
-  Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-  the officially supported framework, also powered by Vite!
+	Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
+	the officially supported framework, also powered by Vite!
 </p>
 
 <style>
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import Counter from "./lib/components/Counter.svelte";
 import NotFound from "./lib/views/NotFound.svelte";
 
 export const routes = {
-  "/": Home,
-  "/counter": Counter,
-  "*": NotFound,
+	"/": Home,
+	"/counter": Counter,
+	"*": NotFound,
 };
