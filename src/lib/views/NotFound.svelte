ddd1dd6a117530c44acefc4929d250d926b1d25d
mbourand <mbourand@student.42.fr>
2022-07-11 20:29:24 +0200

    Svelte SPA setup

---
+++ src/lib/views/NotFound.svelte
@@ -0,0 +1,19 @@
+<h1>Page Not Found</h1>
+
+<style>
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4rem;
+		font-weight: 100;
+		line-height: 1.1;
+		margin: 2rem auto;
+		max-width: 14rem;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import Home from "./lib/views/Home.svelte";
+import Counter from "./lib/components/Counter.svelte";
+import NotFound from "./lib/views/NotFound.svelte";
+
+export const routes = {
+  "/": Home,
+  "/counter": Counter,
+  "*": NotFound,
+};
