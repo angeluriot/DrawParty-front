556d19aba7f621e3d78ed5c619e5eef5242f348c
mbourand <mbourand@student.42.fr>
2022-07-20 05:04:34 +0200

    Opti render

--- src/lib/shared/drawcanvas/BrushPoint.ts
+++ src/lib/shared/drawcanvas/BrushPoint.ts
@@ -5,6 +5,7 @@ export default class BrushPoint {
 	brushSize: number;
 	point: Point;
 	eraser: boolean;
+	pathBegun = false;
 
 	constructor(point: Point, color: string, brushSize: number, eraser: boolean) {
 		this.point = point;
@@ -13,7 +14,33 @@ export default class BrushPoint {
 		this.eraser = eraser;
 	}
 
-	drawLine(canvas, ctx: CanvasRenderingContext2D, otherPoint: BrushPoint): void {
+	beginPath(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
+		if (this.eraser)
+			ctx.globalCompositeOperation = 'destination-out';
+		ctx.lineCap = 'round';
+		ctx.lineJoin = 'round';
+		ctx.strokeStyle = this.color;
+		ctx.lineWidth = this.brushSize;
+		ctx.moveTo(this.point.x * canvas.width, this.point.y * canvas.height);
+		ctx.beginPath();
+		this.pathBegun = true;
+	}
+
+	lineTo(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D, point: Point) {
+		if (!this.pathBegun)
+			this.beginPath(canvas, ctx);
+		ctx.lineTo(point.x * canvas.width, point.y * canvas.height);
+	}
+
+	render(ctx: CanvasRenderingContext2D) {
+		if (!this.pathBegun)
+			return;
+		ctx.stroke();
+		ctx.globalCompositeOperation = 'source-over';
+		this.pathBegun = false;
+	}
+
+	drawLine(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D, otherPoint: BrushPoint): void {
 		if (this.eraser)
 			ctx.globalCompositeOperation = 'destination-out';
 		ctx.lineCap = 'round';
--- src/lib/views/DrawTest.svelte
+++ src/lib/views/DrawTest.svelte
@@ -1,28 +1,14 @@
-<script lang="ts">`
-	/*
-	The algorithm works with two separate lists,
-	one representing the actions confirmed by the server and
-	one representing local changes that aren't confirmed yet.
-	Local actions are drawn over server ones.
-
-	An action is confirmed when it appears in a 'serverUpdate' socket.io event.
-
-	An action is currently just a point with an id representing the path to which the point belongs.
-
-	The server can send different actions during the 'serverUpdate' event :
-	- createBrush, creates the first points of a new path
-	- updateBrush, add points to the current path of the action's player
-
-	The clear action is handled in its own socket.io event 'clearActions'
-	*/
-`
+<script lang="ts">
 	import { onMount, onDestroy } from "svelte";
 	import Global from "../shared/global";
 	import Point from "../shared/Point";
 	import DrawManager from "../shared/drawcanvas/DrawManager";
-import BrushPoint from "../shared/drawcanvas/BrushPoint";
+	import BrushPoint from "../shared/drawcanvas/BrushPoint";
 
-	// 0 = main (layer 1), 1 = layer 2, 2 = sketch
+	export let canDraw = true;
+	export let clientOnly = false;
+
+	// 0 = main (layer 2), 1 = layer 1, 2 = sketch
 	let canvasByLayer: HTMLCanvasElement[] = [undefined, undefined, undefined];
 	let contextByLayer: CanvasRenderingContext2D[] = [undefined, undefined, undefined];
 
@@ -31,7 +17,7 @@ import BrushPoint from "../shared/drawcanvas/BrushPoint";
 	let selectedColor = '#000000';
 	let brushSize = 3;
 
-	// 0 = main (layer 1), 1 = layer 2, 2 = sketch
+	// 0 = main (layer 2), 1 = layer 1, 2 = sketch
 	let selectedLayer = 0;
 
 	let drawManager = new DrawManager();
@@ -71,70 +57,74 @@ import BrushPoint from "../shared/drawcanvas/BrushPoint";
 			contextByLayer[i].translate(0.5, 0.5);
 		drawBackground();
 
-		canvasByLayer[0].addEventListener('mousedown', onMouseDown);
-		canvasByLayer[0].addEventListener('mousemove', onMouseMove);
-		canvasByLayer[0].addEventListener('mouseup', stopDrawing);
+		if (canDraw) {
+			canvasByLayer[0].addEventListener('mousedown', onMouseDown);
+			canvasByLayer[0].addEventListener('mousemove', onMouseMove);
+			canvasByLayer[0].addEventListener('mouseup', stopDrawing);
+		}
 
-		// Triggered every server tick
-		Global.socket.on('serverUpdate', (data: any) => {
-			for (const action of data) {
-				/*
-				Since websockets preserve order, players' server-side actions and local actions are in the same order
-				Therefore, the nth action of the server action list is the nth action of the local player.
-				This allows to splice the first element of the local (unconfirmed) list every time we encounter an action of the local player
-				*/
+		if (!clientOnly) {
+			// Triggered every server tick
+			Global.socket.on('serverUpdate', (data: any) => {
+				for (const action of data) {
+					/*
+					Since websockets preserve order, players' server-side actions and local actions are in the same order
+					Therefore, the nth action of the server action list is the nth action of the local player.
+					This allows to splice the first element of the local (unconfirmed) list every time we encounter an action of the local player
+					*/
 
-				if (action.type != 'undo' && action.type != 'redo' && action.type != 'updateBrush')
-					drawManager.clearUndoStack(action.requestedBy);
+					if (action.type != 'undo' && action.type != 'redo' && action.type != 'updateBrush')
+						drawManager.clearUndoStack(action.requestedBy);
 
-				// Creates a new brush path
-				if (action.type == 'createBrush') {
-					if (action.requestedBy == Global.socket.id) {
-						// Twice for the two points
-						drawManager.confirmAction();
-						drawManager.confirmAction();
-						continue;
-					}
-					const brushPoint = new BrushPoint(new Point(action.data.point.x, action.data.point.y), action.data.color, action.data.size, action.data.eraser);
-					drawManager.createBrush(action.requestedBy, brushPoint, action.data.layer, true, canvasByLayer[action.data.layer], contextByLayer[action.data.layer]);
-					drawManager.moveLastToConfirmPosition();
-					drawManager.moveLastToConfirmPosition();
-				}
-				// Adds a point to the current path of the action's player
-				else if (action.type == 'updateBrush') {
-					for (const point of action.data) {
+					// Creates a new brush path
+					if (action.type == 'createBrush') {
 						if (action.requestedBy == Global.socket.id) {
+							// Twice for the two points
+							drawManager.confirmAction();
 							drawManager.confirmAction();
 							continue;
 						}
-						const lastAction = drawManager.getLastActionOfPlayer(action.requestedBy);
-						drawManager.addPoint(lastAction, new Point(point.x, point.y), true, canvasByLayer[lastAction.layer], contextByLayer[lastAction.layer]);
+						const brushPoint = new BrushPoint(new Point(action.data.point.x, action.data.point.y), action.data.color, action.data.size, action.data.eraser);
+						drawManager.createBrush(action.requestedBy, brushPoint, action.data.layer, true, canvasByLayer[action.data.layer], contextByLayer[action.data.layer]);
+						drawManager.moveLastToConfirmPosition();
 						drawManager.moveLastToConfirmPosition();
 					}
+					// Adds a point to the current path of the action's player
+					else if (action.type == 'updateBrush') {
+						for (const point of action.data) {
+							if (action.requestedBy == Global.socket.id) {
+								drawManager.confirmAction();
+								continue;
+							}
+							const lastAction = drawManager.getLastActionOfPlayer(action.requestedBy);
+							drawManager.addPoint(lastAction, new Point(point.x, point.y), true, canvasByLayer[lastAction.layer], contextByLayer[lastAction.layer]);
+							drawManager.moveLastToConfirmPosition();
+						}
+					}
+					else if (action.type == 'undo') {
+						if (action.requestedBy != Global.socket.id)
+							drawManager.undo(action.requestedBy);
+					}
+					else if (action.type == 'redo') {
+						if (action.requestedBy != Global.socket.id)
+							drawManager.redo(action.requestedBy);
+					}
 				}
-				else if (action.type == 'undo') {
-					if (action.requestedBy != Global.socket.id)
-						drawManager.undo(action.requestedBy);
-				}
-				else if (action.type == 'redo') {
-					if (action.requestedBy != Global.socket.id)
-						drawManager.redo(action.requestedBy);
-				}
-			}
-			if (data.length > 0)
-				render();
-		});
+				if (data.length > 0)
+					render();
+			});
 
-		// Clears all of a player's actions
-		Global.socket.on('clearActions', (data: any) => {
-			drawManager.clearActions(data.requestedBy);
-		});
+			// Clears all of a player's actions
+			Global.socket.on('clearActions', (data: any) => {
+				drawManager.clearActions(data.requestedBy);
+			});
+		}
 	});
 
 	function createBrush(point: Point): void {
 		const brushPoint = new BrushPoint(point, selectedColor, brushSize, selectedTool == 'eraser');
 		drawManager.createBrush(Global.socket.id, brushPoint, selectedLayer, selectedLayer == 2, canvasByLayer[selectedLayer], contextByLayer[selectedLayer]);
-		if (selectedLayer != 2)
+		if (selectedLayer != 2 && !clientOnly)
 			Global.socket.emit('createBrush', { color: selectedColor, size: brushSize, point, eraser: selectedTool == 'eraser', layer: selectedLayer });
 	}
 
@@ -144,9 +134,15 @@ import BrushPoint from "../shared/drawcanvas/BrushPoint";
 	}
 
 	onDestroy(() => {
-		canvasByLayer[0].removeEventListener('mousedown', onMouseDown);
-		canvasByLayer[0].removeEventListener('mousemove', onMouseMove);
-		canvasByLayer[0].removeEventListener('mouseup', stopDrawing);
+		if (canDraw) {
+			canvasByLayer[0].removeEventListener('mousedown', onMouseDown);
+			canvasByLayer[0].removeEventListener('mousemove', onMouseMove);
+			canvasByLayer[0].removeEventListener('mouseup', stopDrawing);
+		}
+		if (!clientOnly) {
+			Global.socket.off('clearActions');
+			Global.socket.off('serverUpdate');
+		}
 	})
 
 	function onMouseDown(e: MouseEvent): void {
@@ -159,7 +155,7 @@ import BrushPoint from "../shared/drawcanvas/BrushPoint";
 		if (!drawing)
 			return;
 		const point = new Point(e.clientX, e.clientY).toRectSpace(canvasByLayer[0].getBoundingClientRect());
-		if (addPoint(point) && selectedLayer != 2)
+		if (addPoint(point) && selectedLayer != 2 && !clientOnly)
 			Global.socket.emit('updateBrush', {points: [point]});
 	}
 
@@ -170,17 +166,18 @@ import BrushPoint from "../shared/drawcanvas/BrushPoint";
 	function clearButton(): void {
 		drawManager.clearActions(Global.socket.id);
 		render();
-		Global.socket.emit('clearActions');
+		if (!clientOnly)
+			Global.socket.emit('clearActions');
 	}
 
 	function undoButton(): void {
-		if (drawManager.undo(Global.socket.id))
+		if (drawManager.undo(Global.socket.id) && !clientOnly)
 			Global.socket.emit('undo');
 		render();
 	}
 
 	function redoButton(): void {
-		if (drawManager.redo(Global.socket.id)) {
+		if (drawManager.redo(Global.socket.id) && !clientOnly) {
 			Global.socket.emit('redo');
 		}
 		render();
@@ -189,12 +186,29 @@ import BrushPoint from "../shared/drawcanvas/BrushPoint";
 	function render() {
 		drawBackground();
 
-		const lastPointPerGroup = new Map<number, number>();
+		const pathStartPerId = new Map<number, number>();
+		let currentId = -1;
 		for (let i = 0; i < drawManager.actions.length; i++) {
-			if (lastPointPerGroup.get(drawManager.actions[i].id) && !drawManager.actions[i].undone) {
-				drawManager.actions[i].data.drawLine(canvasByLayer[drawManager.actions[i].layer], contextByLayer[drawManager.actions[i].layer], drawManager.actions[lastPointPerGroup.get(drawManager.actions[i].id)].data);
+			if (drawManager.actions[i].undone)
+				continue;
+
+			if (currentId != drawManager.actions[i].id) {
+				if (currentId != -1) {
+					const index = pathStartPerId.get(currentId);
+					drawManager.actions[index].data.render(contextByLayer[drawManager.actions[index].layer]);
+				}
+				currentId = drawManager.actions[i].id;
+				pathStartPerId.set(currentId, i);
 			}
-			lastPointPerGroup.set(drawManager.actions[i].id, i);
+
+			if (currentId == drawManager.actions[i].id) {
+				const index = pathStartPerId.get(currentId);
+				drawManager.actions[index].data.lineTo(canvasByLayer[drawManager.actions[index].layer], contextByLayer[drawManager.actions[index].layer], drawManager.actions[i].data.point);
+			}
+		}
+		if (currentId != -1) {
+			const index = pathStartPerId.get(currentId);
+			drawManager.actions[index].data.render(contextByLayer[drawManager.actions[index].layer]);
 		}
 	}
 </script>
@@ -205,17 +219,20 @@ import BrushPoint from "../shared/drawcanvas/BrushPoint";
 		<canvas bind:this={canvasByLayer[1]} width="800" height="600"></canvas>
 		<canvas bind:this={canvasByLayer[0]} width="800" height="600"></canvas>
 	</div>
-	<input type="color" bind:value={selectedColor}>
-	<span>Brush size : {brushSize}</span>
-	<input type="range" min="1" max="30" bind:value={brushSize}>
-	<button on:click={clearButton}>Clear</button>
-	<button on:click={undoButton}>Undo</button>
-	<button on:click={redoButton}>Redo</button>
-	<button on:click={selectBrush}>Brush</button>
-	<button on:click={selectEraser}>Eraser</button>
-	<button on:click={selectSketchLayer}>Sketch</button>
-	<button on:click={selectLayer1}>Layer 1</button>
-	<button on:click={selectLayer2}>Layer 2</button>
+
+	{#if canDraw}
+		<input type="color" bind:value={selectedColor}>
+		<span>Brush size</span>
+		<input type="range" min="1" max="30" bind:value={brushSize}>
+		<button on:click={clearButton}>Clear</button>
+		<button on:click={undoButton}>Undo</button>
+		<button on:click={redoButton}>Redo</button>
+		<button on:click={selectBrush}>Brush</button>
+		<button on:click={selectEraser}>Eraser</button>
+		<button on:click={selectSketchLayer}>Sketch</button>
+		<button on:click={selectLayer1}>Layer 1</button>
+		<button on:click={selectLayer2}>Layer 2</button>
+	{/if}
 </section>
 
 <style>
